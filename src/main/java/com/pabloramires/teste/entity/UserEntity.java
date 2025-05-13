e7fda48a48a0a0fd9275835c5797229cd17c751c
Pramires <pramriesramires86@gmail.com>
2025-05-13 14:40:39 -0300

    Add entities

---
+++ src/main/java/com/pabloramires/teste/entity/UserEntity.java
@@ -0,0 +1,49 @@
+package com.pabloramires.teste.entity;
+
+import com.pabloramires.teste.dto.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "Users")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name = "email",nullable = false)
+    private String email;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AddressEntity> addresses;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
